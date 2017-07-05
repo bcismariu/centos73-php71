aa2432efbb9f074cce424682b9d0977a43799aff
Bogdan Cismariu <bogdan.cismariu@gmail.com>
2017-07-05 19:46:27 +0300

    Added Docker files

---
+++ install-php71.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# PHP 7.1
+#----------------
+
+# Install IUS Repo
+cd ~
+curl 'https://setup.ius.io/' -o setup-ius.sh
+bash setup-ius.sh
+rm setup-ius.sh
+
+
+# Install PHP and dependencies
+yum install -y \
+	php71u-cli \
+	php71u-mysqlnd \
+	php71u-bcmath \
+	php71u-common \
+	php71u-json \
+	php71u-mbstring \
+	php71u-imap \
+	php71u-mcrypt \
+	php71u-xml \
+	php71u-pdo
+
+
